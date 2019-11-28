595783f6092fb2a759559404d8ae91a9c374342b
Ivor <ivor@devpie.io>
2019-11-29 00:51:48 +0100

    feat(createTable): add example of table name options
    There are two ways to change table names
    1. override table name with receiver function, (e.g., func (u User) TableName() string)
    2. remove automatic pluralization of table names (e.g., db.SingularTable(true))

--- examples/postgres/createTable/main.go
+++ examples/postgres/createTable/main.go
@@ -12,6 +12,15 @@ type User struct {
 	LastName  string
 }
 
+// There are two ways to change table names
+// 1. override table name with receiver function, (e.g., func (u User) TableName() string)
+// 2. remove automatic pluralization of table names (e.g., db.SingularTable(true))
+
+// Override table name to something specific
+// func (u User) TableName() string {
+// 	return "test_users"
+// }
+
 func main() {
 	db, err := gorm.Open("postgres", "postgres://ivorscott:@localhost:5432/gorm-usage?sslmode=disable")
 	if err != nil {
@@ -24,6 +33,10 @@ func main() {
 	defer dbase.Close()
 
 	db.DropTable(&User{})
+
+	// remove automatic pluralization of table names
+	// db.SingularTable(true)
+
 	db.CreateTable(&User{})
 
 	println("done")
