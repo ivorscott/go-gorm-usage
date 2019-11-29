dfc092f8688b5d5d682e5262ad75d545530e24a5
Ivor <ivor@devpie.io>
2019-11-29 06:48:58 +0100

    feat(index): add a unique index
    If a field is unique, you can add a unique index.

---
+++ examples/postgres/index/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
+
+type User struct {
+	gorm.Model
+	Username  string `sql:"unique;unique_index"`
+	FirstName string `sql:"size:100"`
+	LastName  string `sql:"type:VARCHAR(15)"`
+	Count     int    `gorm:"AUTO_INCREMENT"`
+	TempField int    `gorm:"-"`
+}
+
+func main() {
+	db, err := gorm.Open("postgres", "postgres://postgres:@localhost:5432/gorm-usage?sslmode=disable")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	dbase := db.DB()
+	defer dbase.Close()
+
+	db.DropTableIfExists(&User{})
+	db.CreateTable(&User{})
+
+	println("done")
+}
